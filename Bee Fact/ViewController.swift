03febbb874e77b7d1dc10c5be6fb1dc93ce3a9ee
Sébastien Kothé <sebastien.kothe@icloud.com>
2020-08-27 21:06:22 +0200

    Initial Commit

---
+++ Bee Fact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bee Fact
+//
+//  Created by Sébastien Kothé on 27/08/2020.
+//  Copyright © 2020 Sébastien Kothé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
